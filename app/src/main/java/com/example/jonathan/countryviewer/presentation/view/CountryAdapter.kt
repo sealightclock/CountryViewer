63e1c1a6353ccca16ffc164e0d9dade3f625f1b7
Sealight Clock <chbebram@hotmail.com>
2024-12-25 13:34:44 -0800

    6th commit: Added item_country.xml to display all data of Country.

--- app/src/main/java/com/example/jonathan/countryviewer/presentation/view/CountryAdapter.kt
+++ app/src/main/java/com/example/jonathan/countryviewer/presentation/view/CountryAdapter.kt
@@ -1,26 +1,30 @@
 package com.example.jonathan.countryviewer.presentation.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.jonathan.countryviewer.databinding.ItemCountryBinding
 import com.example.jonathan.countryviewer.domain.datamodels.Country
 
 class CountryAdapter(private val countries: List<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
-    class CountryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val countryName: TextView = view.findViewById(android.R.id.text1)
+    class CountryViewHolder(private val binding: ItemCountryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(country: Country) {
+            binding.countryName.text = country.name
+            binding.countryRegion.text = country.region
+            binding.countryCode.text = country.code
+            binding.countryCapital.text = country.capital
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(android.R.layout.simple_list_item_1, parent, false)
-        return CountryViewHolder(view)
+        val binding = ItemCountryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CountryViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        holder.countryName.text = countries[position].name
+        holder.bind(countries[position])
     }
 
     override fun getItemCount(): Int = countries.size
