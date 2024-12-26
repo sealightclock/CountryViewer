0b39ebd5126e7ee9bee806c0c5d710da65bd9cfd
Sealight Clock <chbebram@hotmail.com>
2024-12-26 11:39:07 -0800

    11th commit: KtorClient: Handle all errors, exceptions, and edge cases.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CountryViewer
 An Android app that lists all the countries in the world, using Kotlin, RecyclerView, and MVVM Clean architecture.
+
+Programming Exercise
+1. Fetch a list of countries in JSON format from this URL:
+   https://gist.githubusercontent.com/peymano-
+   wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/cou
+   ntries.json
+2. Display all the countries in a RecyclerView ordered by the position they appear in the JSON. In each
+   cell, show the country's "name", "region", "code" and "capital" in this format:
+-----------------------------------------------------
+| |
+| "name", "region" "code" |
+| |
+| "capital" |
+| |
+-----------------------------------------------------
+The user should be able to scroll through the full list of countries.
+Success hints:
+Use MVVM (Model View View Model) + clean architecture
+The implementation must be robust (i.e., handling errors and edge cases) and support device rotation.
+The goal is to write high-quality code for the portion you choose to implement, not the number of
+features implemented.
+Show your best in this minimal project scope.
+Do’s and don’t
+• Use coroutines
+• For the sake of this coding challenge; do not use compose
--- app/src/main/java/com/example/jonathan/countryviewer/data/restful/KtorClient.kt
+++ app/src/main/java/com/example/jonathan/countryviewer/data/restful/KtorClient.kt
@@ -5,10 +5,13 @@ import com.example.jonathan.countryviewer.domain.datamodels.Country
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentLength
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
@@ -16,7 +19,7 @@ import kotlinx.serialization.json.Json
 private const val TAG = "CV: KtorClient: "
 
 /**
- * This singleton is the Ktor client.
+ * This singleton is the Ktor client calling RESTful APIs.
  */
 object KtorClient {
     private val client = HttpClient(CIO) {
@@ -27,6 +30,10 @@ object KtorClient {
             })
         }
 
+        install(HttpTimeout) {
+            requestTimeoutMillis = 5000 // 5 seconds
+        }
+
         install(Logging) {
             level = LogLevel.INFO
         }
@@ -35,46 +42,88 @@ object KtorClient {
     suspend fun getCountries(url: String): List<Country> {
         Log.d(TAG, "getCountries: url=[$url]")
 
+        // Handle response:
         try {
             val httpResponse = client.get(url)
 
-            Log.v(TAG, "getCountries: httpResponse.status=[${httpResponse.status}]")
-            Log.v(TAG, "getCountries: httpResponse=[$httpResponse]")
-
-            val stringBody: String = httpResponse.body()
-
-            Log.v(TAG, "getCountries: stringBody=[$stringBody]")
-
-            // TODO: fix crash:
-            //val countries: List<Country> = httpResponse.body()
-
-            // TODO: This is a workaround to fix the above crash:
-            val json = Json {
-                ignoreUnknownKeys = true
+            // Handle empty content:
+            if (httpResponse.contentLength() == 0L) {
+                Log.e(TAG, "getCountries: contentLength == 0 !!")
+                return listOf(
+                    Country("contentLength == 0", "Please contact developer", "for", "assistance"),
+                )
             }
-            val countries: List<Country> = json.decodeFromString(stringBody)
 
-            Log.v(TAG, "getCountries: [${countries.size}] countries found.")
+            val stringBody: String
+
+            // Handle response status codes:
+            when (httpResponse.status) {
+                HttpStatusCode.OK -> {
+                    stringBody = httpResponse.body<String>()
+                    Log.v(TAG, "getCountries: OK: stringBody=[$stringBody]")
+                }
+                HttpStatusCode.BadRequest -> {
+                    Log.e(TAG, "getCountries: BadRequest !!")
+                    return listOf(
+                        Country("BadRequest", "Please contact developer", "for", "assistance"),
+                    )
+                }
+                HttpStatusCode.InternalServerError -> {
+                    Log.e(TAG, "getCountries: InternalServerError !!")
+                    return listOf(
+                        Country("InternalServerError", "Please contact developer", "for", "assistance"),
+                    )
+                }
+                else -> {
+                    Log.e(TAG, "getCountries: ${httpResponse.status} !!")
+                    return listOf(
+                        Country("${httpResponse.status}", "Please contact developer", "for", "assistance"),
+                    )
+                }
+            }
+
+            var countries: List<Country>
+
+            // Handle deserialization:
+            try {
+                // TODO: This may not work. More investigation will be needed:
+                countries = httpResponse.body()
+
+                Log.v(TAG, "getCountries: 1st method: [${countries.size}] countries found.")
+            } catch (e: Exception) {
+                Log.e(TAG, "getCountries: stackTrace=\n${e.stackTraceToString()}")
+
+                // TODO: This is a back-up solution in case the above call failed:
+                val json = Json {
+                    ignoreUnknownKeys = true
+                }
+
+                // Handle deserialization with backup solution:
+                try {
+                    countries = json.decodeFromString(stringBody)
+                } catch (e: Exception) {
+                    Log.e(TAG, "getCountries: stackTrace=\n${e.stackTraceToString()}")
+                    return listOf(
+                        Country("{2nd method failed}", "Please contact developer", "for", "assistance"),
+                    )
+                }
+
+                Log.v(TAG, "getCountries: 2nd method: [${countries.size}] countries found.")
+            }
 
             return countries
         } catch (e: java.net.SocketException) {
             Log.e(TAG, "getCountries: stackTrace=\n${e.stackTraceToString()}")
-
-            // Inform user of lack of internet connection:
             return listOf(
                 Country("SocketException", "Please connect to internet", "then", "restart the app"),
             )
         } catch (e: java.nio.channels.UnresolvedAddressException) {
             Log.e(TAG, "getCountries: stackTrace=\n${e.stackTraceToString()}")
-
-            // Inform user of lack of internet connection:
             return listOf(
                 Country("UnresolvedAddressException", "Please connect to internet", "then", "restart the app"),
             )
         } catch (e: Exception) { // Other errors:
             Log.e(TAG, "getCountries: stackTrace=\n${e.stackTraceToString()}")
-
-            // Inform user of other errors:
             return listOf(
                 Country("Something went wrong", "Please contact the developer", "for", "assistance"),
             )
